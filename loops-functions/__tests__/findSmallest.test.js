3b58eade3e452b10536619b1fad3cf7983020879
grimnia <grimnia@gmail.com>
2018-09-27 15:50:13 -0400

    Initial comments' '

--- loops-functions/__tests__/findSmallest.test.js
+++ loops-functions/__tests__/findSmallest.test.js
@@ -1,7 +1,7 @@
-xdescribe('Find smallest number tests', () => {
+describe('Find smallest number tests', () => {
 	let findSmallest = require('../exercises/findSmallest');
 	it('should find the smallest number in an array', () => {
-		expect(findSmallest([-1000, 3, 0 ,5])).toBe(-1000);
+		expect(findSmallest([3,-1000, 0 ,5])).toBe(-1000);
 	})
 
 	it('should return null if array is empty', () => {
--- loops-functions/exercises/raiseToThePowerOf.js
+++ loops-functions/exercises/raiseToThePowerOf.js
@@ -1,3 +1,20 @@
+function raiseToThePowerOf(arr, number) {
+  if (arr.length === 0); {
+    return null;
+  }
+for(let i = 0; i < arr.lengh; i++) {
+  arr[i] = arr[i]**number;
+ }
+ return arr;
+}
+
+
+//   }
+//   System.out.printIn(Math.pow(arr,number));
+// }
+
+
+
 
 
 //Ignore this line of code
