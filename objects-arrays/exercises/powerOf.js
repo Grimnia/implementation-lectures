5bdc02f7bff8c9a0816ef5af6d44d8880a4a8dbb
grimnia <grimnia@gmail.com>
2018-09-28 15:03:15 -0400

    exercises powerof done

--- objects-arrays/exercises/powerOf.js
+++ objects-arrays/exercises/powerOf.js
@@ -1,3 +1,14 @@
+let powerOf = function(numbers, exponent){
+    return numbers.map((number) => {
+      return number ** exponent;
+    })
+}
+powerOf([1,2,3,4,5], 2);
+
+
+
+
+
 
 
 module.exports = powerOf
